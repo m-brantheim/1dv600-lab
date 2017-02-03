a79dc665b233344a59fbc6d37bd2320c75eb9425
Magnus Brantheim <m.brantheim@gmail.com>
2017-02-03 22:41:54 +0100

    Added a book factory function which returns a book object.

--- app/resources/GetBooksResource.js
+++ app/resources/GetBooksResource.js
@@ -3,9 +3,18 @@
 
     var LibraryDAO = require('../dao/LibraryDAO');
 
+    const book = {
+      id: 1,
+      title: "yo",
+      author: "Kimboll",
+      genre: "Stooufzman",
+      publishDate: "2202020",
+      price: 555,
+      description: "One day man went out of woods then came back"
+    }
 
     module.exports = function (callback, title) { // The title is optional and is only present when searching. (You need yo modify the books.js file first)
-
+      callback(book);
     };
 
 }());
